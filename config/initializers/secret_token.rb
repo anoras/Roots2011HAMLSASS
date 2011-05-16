b4b288c15d2bcef8882e266629aa8e8e84664983
Anders Norås <anders@snehvit.local>
2011-05-16 11:02:31 +0200

    Plain Rails 3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roots2011HAMLSASS::Application.config.secret_token = 'f2c1002f1ec8a6dd4d47d012bd2a9748a2e5cb417a22381f88e37e6369b74aba0b4be119e58492f8176d225982d3082a786a42bb99301dfc26b02b0db2927372'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Roots2011HAMLSASS::Application.config.session_store :cookie_store, :key => '_Roots2011HAMLSASS_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Roots2011HAMLSASS::Application.config.session_store :active_record_store
